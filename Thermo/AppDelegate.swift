80b957618ef88e4a6c3f63848001315a8731bde2
Naoya Sato <s@tonaoya.com>
2014-10-19 02:25:48 +0900

    Initial Commit

---
+++ Thermo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Thermo
+//
+//  Created by Sato Naoya on 10/19/14.
+//  Copyright (c) 2014 Sato Naoya. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
